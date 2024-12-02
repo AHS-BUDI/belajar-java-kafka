bb81544d7d88257551e967bfe46c52b65e3897a5
budiyyy <budi.yusuf@adis.co.id>
2024-12-02 11:22:21 +0700

    Initial commit

---
+++ src/test/java/odank/kafka/BelajarJavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package odank.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
